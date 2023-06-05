27d7013a42dae1cac8167705cbd8ee9f86bb1832
CrossStack-Q <crossstack@gmail.com>
2023-06-05 21:51:43 +0530

    start_again

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,27 +7,34 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 
 
 const Board = () => {
-   const getBoard = useBoardStore((state) => state.getBoard);
+   const [board,getBoard] = useBoardStore((state) => [state.board,state.getBoard]);
 
   useEffect(()=> {
     getBoard();
   }, [getBoard])
+
+  console.log(board);
   
 
   return (
-    <h1>
-      Anurag
-    </h1>
-    // <DragDropContext >
-    //   <Droppable droppableId="board" direction="horizontal" type="column">
-    //     {(provided) => (
-    //       <div>
-    //         {/* rendering blocks okay */}
-    //         Hello
-    //       </div>
-    //     )}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext >
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div>
+            {/* rendering blocks okay */}
+            {Array.from(board.columns.entries()).map(([id,column],index) => (
+              <div
+              {...provided.droppableProps}
+              ref={provided.innerRef }
+              >
+                <Column key={id} id={id} todos={column.todos} index={index} />
+              </div>
+            ))}
+            Hello
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
